4a8ef7810525537aeafe64185a254a0ef41aa7f0
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-03 02:18:31 +0530

    did some chenges Configuration.java

--- src/main/java/org/example/Configuration.java
+++ src/main/java/org/example/Configuration.java
@@ -1,72 +1,34 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.*;
-
 public class Configuration {
-    private int totalTickets = 50; // Default total tickets
-    private int poolCapacity = 10; // Default pool capacity
-    private int vendorInterval = 1000; // Interval in ms
-    private int customerInterval = 800; // Interval in ms
+    private int totalTickets = 1000;
+    private int maxCapacity = 1000;
+    private int releaseRate = 1000;  // milliseconds for vendors
+    private int retrievalRate = 500; // milliseconds for customers
 
-    // Getters and Setters
-    public int getTotalTickets() { return totalTickets; }
-    public void setTotalTickets(int totalTickets) {
-        if(totalTickets <= 0) {
-            System.out.println("Total tickets must be greater than 0!");
-        } else {
-            this.totalTickets = totalTickets;
-        }
+    // Method to load configuration (can be extended to read from a file)
+    public void loadConfig() {
+        // Hardcoded values for simplicity
+        this.totalTickets = 1000;
+        this.maxCapacity = 1000;
+        this.releaseRate = 1000;
+        this.retrievalRate = 500;
     }
 
-    public int getPoolCapacity() { return poolCapacity; }
-    public void setPoolCapacity(int poolCapacity) {
-        if(poolCapacity <= 0) {
-            System.out.println("Pool capacity must be greater than 0!");
-        } else {
-            this.poolCapacity = poolCapacity;
-        }
+    public int getTotalTickets() {
+        return totalTickets;
     }
 
-    public int getVendorInterval() { return vendorInterval; }
-    public void setVendorInterval(int vendorInterval) {
-        if(vendorInterval <= 0) {
-            System.out.println("Vendor interval must be greater than 0!");
-        } else {
-            this.vendorInterval = vendorInterval;
-        }
+    public int getMaxCapacity() {
+        return maxCapacity;
     }
 
-    public int getCustomerInterval() { return customerInterval; }
-    public void setCustomerInterval(int customerInterval) {
-        if(customerInterval <= 0) {
-            System.out.println("Customer interval must be greater than 0!");
-        } else {
-            this.customerInterval = customerInterval;
-        }
+    public int getReleaseRate() {
+        return releaseRate;
     }
 
-    // Serialization to JSON format
-    public void saveConfigurationToJSON(String filename) {
-        try (FileWriter writer = new FileWriter(filename)) {
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            gson.toJson(this, writer);
-            System.out.println("Configuration saved to " + filename);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Load configuration from JSON file
-    public static Configuration loadConfigurationFromJSON(String filename) {
-        try (FileReader reader = new FileReader(filename)) {
-            Gson gson = new Gson();
-            return gson.fromJson(reader, Configuration.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public int getRetrievalRate() {
+        return retrievalRate;
     }
 }
 
