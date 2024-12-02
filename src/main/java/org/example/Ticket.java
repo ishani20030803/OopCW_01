56297c92bfda3e7cbc851c17e15282d0875ceb1b
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-03 02:15:43 +0530

    did some chenges Ticket.java

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -1,21 +1,15 @@
 package org.example;
 
 public class Ticket {
-    private int ticketId;
-    private String eventName;
-    private int ticketPrice;
+    private static int idCounter = 0;
+    private final int ticketId;
 
-    public Ticket(int ticketId, String eventName, int ticketPrice) {
-        this.ticketId = ticketId;
-        this.eventName = eventName;
-        this.ticketPrice = ticketPrice;
+    public Ticket() {
+        this.ticketId = idCounter++;
     }
 
-    @Override
-    public String toString() {
-        return "Ticket{ticketId=" + ticketId +
-                ", eventName='" + eventName + '\'' +
-                ", ticketPrice=" + ticketPrice + '}';
+    public int getTicketId() {
+        return ticketId;
     }
 }
 
