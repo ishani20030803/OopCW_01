4885a7696bca3c7922710ffdbd9c4a6e6bd4e2fe
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-03 12:00:18 +0530

    chenge the Configuration.java classe

--- src/main/java/org/example/Configuration.java
+++ src/main/java/org/example/Configuration.java
@@ -1,18 +1,16 @@
 package org.example;
 
 public class Configuration {
-    private int totalTickets = 1000;
-    private int maxCapacity = 1000;
-    private int releaseRate = 1000;  // milliseconds for vendors
-    private int retrievalRate = 500; // milliseconds for customers
+    private int totalTickets;
+    private int maxCapacity;
+    private int releaseRate; // in milliseconds
+    private int retrievalRate; // in milliseconds
 
-    // Method to load configuration (can be extended to read from a file)
-    public void loadConfig() {
-        // Hardcoded values for simplicity
-        this.totalTickets = 1000;
-        this.maxCapacity = 1000;
-        this.releaseRate = 1000;
-        this.retrievalRate = 500;
+    public Configuration(int totalTickets, int maxCapacity, int releaseRate, int retrievalRate) {
+        this.totalTickets = totalTickets;
+        this.maxCapacity = maxCapacity;
+        this.releaseRate = releaseRate;
+        this.retrievalRate = retrievalRate;
     }
 
     public int getTotalTickets() {
