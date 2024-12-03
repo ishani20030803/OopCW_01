cc00e7ab41c44e7fa9900de5d9ebb183a2b6f38b
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-03 11:48:43 +0530

    chenge the Logger.java classe

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -1,20 +1,9 @@
 package org.example;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Logger {
-    private static final String LOG_FILE = "system_log.txt";
-
     public static void log(String message) {
-        System.out.println(message); // Log to console
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
-            writer.write(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + ": " + message);
-            writer.newLine();
-        } catch (IOException e) {
-            System.err.println("Error writing log: " + e.getMessage());
-        }
+        System.out.println(message);
     }
 }
+
 
