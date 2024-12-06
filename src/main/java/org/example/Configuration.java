aaab8dfa12eb7a539d281c62287035c7113a4569
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-06 21:13:31 +0530

    chenge the congfiguratin.java file

--- src/main/java/org/example/Configuration.java
+++ src/main/java/org/example/Configuration.java
@@ -1,32 +1,9 @@
 package org.example;
 
 public class Configuration {
-    private int totalTickets;
-    private int maxCapacity;
-    private int releaseRate; // in milliseconds
-    private int retrievalRate; // in milliseconds
-
-    public Configuration(int totalTickets, int maxCapacity, int releaseRate, int retrievalRate) {
-        this.totalTickets = totalTickets;
-        this.maxCapacity = maxCapacity;
-        this.releaseRate = releaseRate;
-        this.retrievalRate = retrievalRate;
-    }
-
-    public int getTotalTickets() {
-        return totalTickets;
-    }
-
-    public int getMaxCapacity() {
-        return maxCapacity;
-    }
-
-    public int getReleaseRate() {
-        return releaseRate;
-    }
-
-    public int getRetrievalRate() {
-        return retrievalRate;
-    }
+    public static final int DEFAULT_MAX_CAPACITY = 5;
+    public static final int DEFAULT_RELEASE_RATE_MS = 2000;
+    public static final int DEFAULT_RETRIEVAL_RATE_MS = 3000;
 }
+
 
