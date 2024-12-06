60f035aec878db2d094ab4ac049fa317deb1ecef
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-06 21:14:44 +0530

    chenge the logger.java file

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -1,9 +1,10 @@
 package org.example;
 
 public class Logger {
-    public static void log(String message) {
+    public static synchronized void log(String message) {
         System.out.println(message);
     }
 }
+
 
 
