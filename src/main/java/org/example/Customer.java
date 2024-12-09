3aa9457c3e1ab3751eb67988d98b58498736d13f
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-09 12:00:52 +0530

    Chenge to did some changes Customer.java class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -4,6 +4,7 @@ public class Customer implements Runnable {
     private final TicketPool ticketPool;
     private final int retrievalRateMs;
     private final String name;
+    private volatile boolean running = true; // Flag to stop thread
 
     public Customer(TicketPool ticketPool, int retrievalRateMs, String name) {
         this.ticketPool = ticketPool;
@@ -13,18 +14,22 @@ public class Customer implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
+        while (running) {
             try {
                 Ticket ticket = ticketPool.retrieveTicket();
                 System.out.println(name + " bought ticket: " + ticket);
                 Thread.sleep(retrievalRateMs);
             } catch (InterruptedException e) {
-                break;
+                break; // Gracefully exit when interrupted
             }
         }
+        System.out.println(name + " stopped.");
+    }
+
+    public void stop() {
+        running = false; // Set flag to stop the thread
     }
 }
-
 
 
 
