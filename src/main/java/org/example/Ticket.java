f15bf45be484a928022f0114d0c71582248ab9ef
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-12 05:54:11 +0530

    Chenge to did some changes Ticket.java class

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -2,10 +2,13 @@ package org.example;
 
 public class Ticket {
     private final int ticketId;
-    private final String eventName;
+    private final String eventName;// The event name that is linked to the ticket.
+
+
     private final double ticketPrice;
 
     public Ticket(int ticketId, String eventName, double ticketPrice) {
+        // Constructor to set the properties of a new `Ticket` object.
         this.ticketId = ticketId;
         this.eventName = eventName;
         this.ticketPrice = ticketPrice;
@@ -13,11 +16,12 @@ public class Ticket {
 
     @Override
     public String toString() {
+        // Provides a string representation of the `Ticket` by overriding the `toString` method.
         return "Ticket{" +
                 "ticketId=" + ticketId +
                 ", eventName='" + eventName + '\'' +
                 ", ticketPrice=" + ticketPrice +
-                '}';
+                '}';// Provides a formatted string with the price, event name, and ticket ID.
     }
 }
 
