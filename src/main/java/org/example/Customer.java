9e46f6c537c0fabd0e08bded4741ffda8ddeef3f
ishani20030803 <ishanidasunika441@gmail.com>
2024-12-12 05:57:27 +0530

    Chenge to did some changes Customer.java class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,14 +1,22 @@
-package org.example;
+package org.example;// Defines the package that contains this class.
+
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Customer implements Runnable {
+    // Declares the `Customer` class, which enables instances to
+    //execute in a different thread by implementing the `Runnable` interface.
     private final TicketPool ticketPool;
+    // A reference to the shared `TicketPool` object that will be used to have tickets
     private final int retrievalRateMs;
     private final String name;
     private final List<Ticket> purchasedTickets = new ArrayList<>();
-    private volatile boolean running = true; // Flag to stop thread
+    private volatile boolean running = true;
+    // A signal to show if the thread should keep running. {volatile` is marked to provide thread safety.
+
+
 
     public Customer(TicketPool ticketPool, int retrievalRateMs, String name) {
         this.ticketPool = ticketPool;
@@ -18,11 +26,14 @@ public class Customer implements Runnable {
 
     @Override
     public void run() {
-        while (running) {
+        while (running) {// Continue running as long as the `running` variable is set to true.
+
+
             try {
                 Ticket ticket;
                 ticket = ticketPool.retrieveTicket();
                 if (ticket != null) {
+                    // Check whether a ticket was obtained successfully.
                     purchasedTickets.add(ticket);
                     System.out.println(name + " bought ticket: " + ticket);
                 }
@@ -38,11 +49,16 @@ public class Customer implements Runnable {
         running = false; // Set flag to stop the thread
     }
 
-    public String getName() {
+    public String getName() {// To stop the `run` loop, set the `running` flag to false.
+
         return name;
     }
 
     public List<Ticket> getPurchasedTickets() {
+        // The list of tickets that have been purchased is obtained.
+
+
+
         return purchasedTickets;
     }
 }
